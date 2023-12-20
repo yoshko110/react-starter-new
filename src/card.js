eea2d1142042388ae89defe7e779b12f0f306b15
“Yousef” <king_of_sword3@hotmail.com>
2023-12-20 18:32:11 +0300

    done

---
+++ src/card.js
@@ -0,0 +1,16 @@
+export function Card() {
+  return (
+    <div>
+      <h1>Yousef Alkhayyat</h1>
+      <h3> KNPC</h3>
+    </div>
+  );
+}
+export function CardSport() {
+  return (
+    <div>
+      <h3>Fencing</h3>
+      <p>Epee</p>
+    </div>
+  );
+}
