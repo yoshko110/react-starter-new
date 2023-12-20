0afd848d3df093e27f374115d07aeda03dbf47a4
“Yousef” <king_of_sword3@hotmail.com>
2023-12-20 20:47:44 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { Card, CardSport } from "./card";
 import Fencing from "./DnoAXCqVsAATHIO.png";
-
+import { swords } from "./ProdictsSwords";
 function App() {
   return (
     <div>
-      <Card></Card>
+      <Card />
       <CardSport></CardSport>
+      <Prodicts></Prodicts>
+
       <img src={Fencing} width="50%"></img>
+    </div>
+  );
+}
+function Prodicts() {
+  return (
+    <div>
+      {swords.map((sword) => (
+        <>
+          <img src={sword.imgFoto} width="15%" />
+          <h1>{sword.descrapt}</h1>
+          <p>{sword.price}</p>
+        </>
+      ))}
     </div>
   );
 }
---
+++ src/ProdictsSwords.js
@@ -0,0 +1,15 @@
+import foil from "./Foil.jpg";
+import epee from "./Epee.jpg";
+
+export const swords = [
+  {
+    imgFoto: foil,
+    descrapt: "Foil",
+    price: "100$",
+  },
+  {
+    imgFoto: epee,
+    descrapt: "Epee",
+    price: "150$",
+  },
+];
